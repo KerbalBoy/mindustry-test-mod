8d36eeb8bed3cf398b05e587d44b4c832ea394b9
KerbalBoy <miles.sluyter@gmail.com>
2021-06-03 22:38:00 +0100

    Initial commit

---
+++ src/io/github/kerbalboy/TestMod.java
@@ -0,0 +1,36 @@
+package io.github.kerbalboy;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class TestMod extends Mod{
+
+    public TestMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name
+                dialog.cont.image(Core.atlas.find("test-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+		Log.info("Loading some example content.");
+    }
+
+}
