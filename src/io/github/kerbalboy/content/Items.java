29ebc4bd354500fc50652e6fde425434039ac666
KerbalBoy <miles.sluyter@gmail.com>
2021-06-10 17:03:18 +0100

    Add red to Items.java

--- src/io/github/kerbalboy/content/Items.java
+++ src/io/github/kerbalboy/content/Items.java
@@ -1,11 +1,16 @@
 package io.github.kerbalboy.content;
 
+import arc.graphics.Color;
 import mindustry.ctype.ContentList;
+import mindustry.type.Item;
 
 public class Items implements ContentList {
 
-	//public static Item
+	public static Item
+		red;
 	
 	@Override
-	public void load() {}
+	public void load() {
+		red = new Item("red", Color.valueOf("AA0000"));
+	}
 }
